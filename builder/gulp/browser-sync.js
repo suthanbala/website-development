a25920b9b2d0834c1c3f65c7ac0c1cea5dd47d29
Suthan <suthan.bb@gmail.com>
2021-08-10 00:52:00 -0400

    Update browser-sync.js
    Added the server includes into the page. You can now include sections using `@include()` directives.

--- builder/gulp/browser-sync.js
+++ builder/gulp/browser-sync.js
@@ -1,15 +1,39 @@
 const path = require("path");
+const fs = require('fs');
 
 function reload(server, done) {
   server.reload();
   done();
 }
 
+const rootPath = path.resolve("../../");
+
 function serve(server, done) {
   server.init({
     server: {
-      baseDir: path.resolve("../../")
+      baseDir: rootPath
     },
+    rewriteRules: [
+      {
+          match: /@include\("(.+?)"\)/g,
+          fn: function (req, res, match) {
+            const pattern = /@include\("(.+?)"\)/gm;
+            let filename;
+            while ((m = pattern.exec(match)) !== null) {
+              // This is necessary to avoid infinite loops with zero-width matches
+                if (m.index === pattern.lastIndex) {
+                    pattern.lastIndex++;
+                }
+                filename = path.resolve(rootPath, m[1]);
+              }
+              if (fs.existsSync(filename)) {
+                  return fs.readFileSync(filename);
+              } else {
+                  return '<span style="color: red">'+filename+' could not be found</span>';
+              }
+          }
+      }
+  ]
   });
   done();
 }
